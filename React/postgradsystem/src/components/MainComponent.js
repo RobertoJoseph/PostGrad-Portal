58af2900337ae14faea425a42f37a22c0b1c0568
Roberto <roberto.josephselim@gmail.com>
2022-02-24 01:24:05 +0200

    add: base project

---
+++ React/postgradsystem/src/components/MainComponent.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+class Main extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+
+    }
+}
+export default Main;
---
+++ React/postgradsystem/src/components/RegisterComponent.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle
+} from 'reactstrap';
+ const Register = (props) => {
+    return (
+        <div className="container">
+            <div className="row justify-content-center align-items-center">
+                <div className="col-12 col-md-6">
+                    <Card >
+                        <CardBody>
+                            <CardTitle>Hablozlo</CardTitle>
+                            <CardText>KakaMe3eze</CardText>
+                        </CardBody>
+                    </Card>
+                </div>
+                <div className="col-12 col-md-6">
+                    <Card >
+                        <CardBody>
+                            <CardTitle>Hablozlo</CardTitle>
+                            <CardText>KakaMe3eze</CardText>
+                        </CardBody>
+                    </Card>
+                </div>
+            </div>
+
+        </div>
+    );
+
+
+}
+export default Register
