9138343c3c5ae1f508c2409c3522b5a0d1c14286
Kareem Heidar <k.abukhater@gmail.com>
2022-02-24 19:04:05 +0200

    edit: improved the homepage design

--- React/postgradsystem/src/components/RegisterComponent.js
+++ React/postgradsystem/src/components/RegisterComponent.js
@@ -1,32 +1,28 @@
 import React from 'react'
-import {
-    Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle
-} from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-  import { faSignIn,faUserPlus } from '@fortawesome/free-solid-svg-icons'
+import { faSignIn, faUserPlus } from '@fortawesome/free-solid-svg-icons'
+
  const Register = (props) => {
     return (
-        <div className="container">
+        <div className="container body">
             <div className="row">
                 <div className="col-12 col-md-6">
 
                     <Card className="button" >
-                <FontAwesomeIcon icon={faSignIn} size="10x"></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faSignIn} size="10x" className='icons'></FontAwesomeIcon>
                         <CardBody>
-                            <CardTitle>Hablozlo</CardTitle>
-                            <CardText>KakaMe3eze</CardText>
+                            <CardTitle className="cardTitle-register"><br></br><br></br><h1>Login In</h1></CardTitle>
                         </CardBody>
                     </Card>
                 </div>
                 <div className="col-12 col-md-6">
                     <Card className="button" >
                         
-                <FontAwesomeIcon icon={faUserPlus} size="10x"></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faUserPlus} size="10x" className='icons'></FontAwesomeIcon>
                   
                         <CardBody>
-                            <CardTitle>Hablozlo</CardTitle>
-                            <CardText>KakaMe3eze</CardText>
+                        <CardTitle className="cardTitle-register"><br></br><br></br><h1>Sign Up</h1></CardTitle>
                         </CardBody>
                     </Card>
                 </div>
--- React/postgradsystem/src/index.js
+++ React/postgradsystem/src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.css';
-import 'bootstrap-social/bootstrap-social.css';
 
 ReactDOM.render(
   <React.StrictMode>
