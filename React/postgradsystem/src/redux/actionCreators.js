3b5b59eae249563c8ea6ce0d8718094438c58ce2
Roberto <roberto.josephselim@gmail.com>
2022-03-27 14:14:25 +0200

    editing actionCreators and MainCompoennt

--- React/postgradsystem/src/redux/actionCreators.js
+++ React/postgradsystem/src/redux/actionCreators.js
@@ -1,58 +1,55 @@
-import * as ActionTypes from './ActionTypes';
+import Axios from "axios";
 
+export const getStudents = () => async (dispatch) => {
+  return Axios.get("http://localhost:9000/students")
+    .then((response) => {
+      //response.data.haga takecare
+      console.log(response.data);
+      dispatch(showStudents(response.data));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
-export const studentRegister = (firstName, lastName, email, password, faculty, address, gucian) => ({
-    type: ActionTypes.STUDENT_REGISTER,
-    payload: {
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        password: password,
-        faculty: faculty,
-        address: address,
-        gucian: gucian
-    }
+export const showStudents = (students) => ({
+  type: "SHOW_STUDENTS",
+  payload: students,
 });
 
-export const addStudent = (firstName, lastName, email, password, faculty, address, gucian) => {
+export const addStudent =
+  (firstName, lastName, email, password, faculty, address, isGucian) =>
+  (dispatch) => {
+    console.log("HAHAHAHahh in the action");
     const newStudent = {
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        password: password,
-        faculty: faculty,
-        address: address,
-        gucian: gucian
-    }
-    return fetch("http://localhost:9000/testApi", {
-        method: "POST",
-        body: JSON.stringify(newStudent),
-        headers: {
-            "Content-Type": "application/json",
-        },
-        credentials: "same-origin",
-    })
-        .then(
-            (response) => {
-                if (response.ok) return response;
-                else {
-                    var error = new Error(
-                        "Error " + response.status + ": " + response.statusText
-                    );
-                    error.response = response;
-                    throw error;
-                }
-            },
-            (error) => {
-                var errMess = new Error(error.message)
-                throw error;
-            }
-        )
-        .then((response) => response.json())
-        .then((response) => {
-            alert("Thank you for your FeedBack\n: " + JSON.stringify(response));
-        })
-        .catch((error) => {
-            alert("Your Feedback could not be posted\nError: " + error.message);
-        });
-};
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password,
+      faculty: faculty,
+      address: address,
+      isGucian: isGucian,
+    };
+    return Axios.post("http://localhost:9000/students/addStudent", newStudent)
+      .then((res) => {
+        alert("Successfuly Added an Student");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+// export const loginStudent = (email, password) => async (dispatch) => {
+//   return Axios.post("http://localhost:9000/students/login", {
+//     email: email,
+//     password: password,
+//   })
+//     .then((response) => {
+      
+//       console.log(response.data);
+//       dispatch(loginStudentSuccess(response.data));
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// };
