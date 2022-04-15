b5023ce86184643fc91b5aa0aa61108a3e7bf517
Roberto <roberto.josephselim@gmail.com>
2022-04-15 14:35:46 +0200

    add new procs to newProcedures file

--- api/routes/students.js
+++ api/routes/students.js
@@ -1,19 +1,19 @@
-var express = require("express");
-var router = express.Router();
-var config = require("../operations/dbconfig");
-const sql = require("mssql");
-var studentOperations = require("../operations/studentoperations");
+// var express = require("express");
+// var router = express.Router();
+// var config = require("../operations/dbconfig");
+// const sql = require("mssql");
+// var studentOperations = require("../operations/studentoperations");
 
-router.get("/", async function (req, res) {
-  try {
-    let pool = await sql.connect(config);
-    const result = await pool.request().query("Select * from GucianStudent");
-    console.log("I am in the router and get all students");
-    res.send(result.recordsets);
-  } catch (error) {
-    console.log(error);
-  }
-});
+// router.get("/", async function (req, res) {
+//   try {
+//     let pool = await sql.connect(config);
+//     const result = await pool.request().query("Select * from GucianStudent");
+//     console.log("I am in the router and get all students");
+//     res.send(result.recordsets);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 router.post("/addStudent", studentOperations.StudentRegister);
 
 router.get(
