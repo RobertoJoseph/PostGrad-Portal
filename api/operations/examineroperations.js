1129ed356f9058231d361036dd68e2c96f142ad7
Roberto <roberto.josephselim@gmail.com>
2022-04-16 22:21:41 +0200

    merge conflicts

--- api/operations/examineroperations.js
+++ api/operations/examineroperations.js
@@ -1,133 +1,55 @@
 var config = require("./dbconfig");
 const sql = require("mssql");
 
-async function editExaminerName() {
-  try {
-    let pool = await sql.connect(config);
-    const result = await pool.request().execute(`editExaminerName`);
-    sql.close();
-  } catch (erorr) {
-    console.log(erorr);
-    sql.close();
-  }
-}
-
-async function editExaminerField() {
-  try {
-    let pool = await sql.connect(config);
-    const result = await pool.request().execute(`editExaminerField`);
-    sql.close();
-  } catch (erorr) {
-    console.log(erorr);
-    sql.close();
-  }
-}
-
-async function editExaminerEmail() {
-  try {
-    let pool = await sql.connect(config);
-    const result = await pool.request().execute(`editExaminerEmail`);
-    sql.close();
-  } catch (erorr) {
-    console.log(erorr);
-    sql.close();
-  }
-}
-
-async function editExaminerPassword() {
-  try {
-    let pool = await sql.connect(config);
-    const result = await pool.request().execute(`editExaminerPassword`);
-    sql.close();
-  } catch (erorr) {
-    console.log(erorr);
-    sql.close();
-  }
-}
-
-async function AddCommentsGrade(ThesisSerialNo, DefenseDate, comments) {
+exports.getExaminerData = async function (req, res) {
   try {
     let pool = await sql.connect(config);
     const result = (
       await pool
         .request()
-        .input("ThesisSerialNo", sql.Int, ThesisSerialNo)
-        .input("DefenseDate", sql.Date, DefenseDate)
-        .input("comments", sql.VarChar, comments)
-        .execute(`AddCommentsGrade`)
+        .input("examinerId", sql.Int, req.params.examinerID)
+        .execute(`ExaminerProfile`)
     ).recordset;
     console.log(result);
-    sql.close();
-    return result;
-  } catch (erorr) {
-    console.log(erorr);
+    res.send(result);
+  } catch (err) {
+    console.log(err);
     sql.close();
   }
-}
+};
+exports.changeExaminerInformation = async function (req, res) {
+  try {
+    let pool = await sql.connect(config);
+    const result = await pool
+      .request()
+      .input("examinerID", sql.Int, req.params.examinerID)
+      .input("oldPassword", sql.VarChar, req.body.oldPassword)
+      .input("newPassword", sql.VarChar, req.body.newPassword)
+      .output("Success", sql.Bit)
+      .execute(`editExaminerPassword`);
+    if (result.output.Success) {
+      res.send({ isUpdated: true });
+    } else {
+      res.send({ isUpdated: false });
+    }
+  } catch (err) {
+    console.log(err);
+    sql.close();
+  }
+};
 
-async function AddDefenseGrade(ThesisSerialNo, DefenseDate, grade) {
+exports.examinerEvaluateDefense = async function (req, res) {
   try {
     let pool = await sql.connect(config);
     const result = (
       await pool
         .request()
-        .input("ThesisSerialNo", sql.Int, ThesisSerialNo)
-        .input("DefenseDate", sql.Date, DefenseDate)
-        .input("grade", sql.Decimal, grade)
-        .execute(`AddDefenseGrade`)
+        .input("id", sql.Int, req.params.examinerID)
+        .execute(`ExaminerAttendedDefense`)
     ).recordset;
-    console.log(result);
-    sql.close();
-    return result;
-  } catch (erorr) {
-    console.log(erorr);
+    res.send(result);
+  } catch (err) {
+    console.log(err);
     sql.close();
   }
-}
-
-async function searchThesis(searchkey) {
-  try {
-    let pool = await sql.connect(config);
-    const result = (
-      await pool
-        .request()
-        .input("searchkey", sql.VarChar, searchkey)
-        .execute(`searchThesis`)
-    ).recordset;
-    console.log(result);
-    sql.close();
-    return result;
-  } catch (erorr) {
-    console.log(erorr);
-    sql.close();
-  }
-}
-
-async function examinerAttendedDefense(id) {
-  try {
-    let pool = await sql.connect(config);
-    const result = (
-      await pool
-        .request()
-        .input("id", sql.Int, id)
-        .execute(`examinerAttendedDefense`)
-    ).recordset;
-    console.log(result);
-    sql.close();
-    return result;
-  } catch (erorr) {
-    console.log(erorr);
-    sql.close();
-  }
-}
-
-module.exports = {
-  editExaminerName,
-  editExaminerField,
-  editExaminerEmail,
-  editExaminerPassword,
-  AddCommentsGrade,
-  AddDefenseGrade,
-  searchThesis,
-  examinerAttendedDefense,
 };
---
+++ api/routes/examiner.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var router = express.Router();
+var config = require("../operations/dbconfig");
+const sql = require("mssql");
+var examinerOperations = require("../operations/examineroperations");
+
+router.get("/examinerdata/:examinerID", examinerOperations.getExaminerData);
+router.post(
+  "/changeinformation/:examinerID",
+  examinerOperations.changeExaminerInformation
+);
+router.get(
+  "/attenddefense/:examinerID",
+  examinerOperations.examinerEvaluateDefense
+);
+module.exports = router;
